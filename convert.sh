4e813c3c170b2f4d628f1e623b0f337150fc7634
meganokeefe <megan037@gmail.com>
2016-08-24 16:37:49 -0700

    drone2

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config remote.origin.url "https://ac66ec5807a1093dd6eca84ac7d091f453b0d49b@github.com/CiscoCloud/vaquero-docs.git"
+git config remote.origin.url "https://20898fe2a25787143e943587d357c91f9d434b58@github.com/CiscoCloud/vaquero-docs.git"
 git config --global user.name gem-test
 git config --global user.email gemini.atlas@gmail.com
 git config --list
