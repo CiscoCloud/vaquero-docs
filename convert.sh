ae1969eb368ef8ca8bd07cac88bdf3bd9ea49b65
Ben Schumacher <me@benschumacher.com>
2016-08-27 11:47:29 -0600

    Reworked conversion and publish to gh-pages branch (#6)

--- convert.sh
+++ convert.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-rm docs/current/*.html #ensure that the conversions output new files
-RECENT="$(git describe --abbrev=0 --tags)"
-git checkout gh-pages
-curl https://api.github.com/repos/CiscoCloud/vaquero-docs/tags > tags.json
-if [ ! -d $RECENT ]; then
-  FILES=docs/current/*.md
-  for f in $FILES
-  do
-    echo Converting $f...
-    pandoc $f -o $f.html
-  done
-  mkdir docs/$RECENT #create version dir
-  cp docs/current/*html docs/$RECENT/
-  cp docs/current/*png docs/$RECENT/
-  cp docs/current/*jpg docs/$RECENT/
+set -ue
+
+TMP=$(mktemp -d -p "/drone")
+git clone -b gh-pages $(git config remote.origin.url) "${TMP}"
+
+TARGET="${TMP}/docs/current"
+if [[ $DRONE_BRANCH != master ]]; then
+    TARGET="${TMP}/docs/branches/${DRONE_BRANCH/\//--}"
+elif [[ -n ${DRONE_TAG:-} ]]; then
+    TARGET="${TMP}/docs/${DRONE_TAG}"
 fi
-git add .
-git commit -m "Pushed $RECENT to site"
-git push origin master
+
+mkdir -vp "${TARGET}"
+for src in docs/current/*.md; do
+    dst="${TARGET}/$(basename "${src}" .md).html"
+    echo Converting ${src} to ${dst}...
+    pandoc "${src}" -o "${dst}"
+done
+
